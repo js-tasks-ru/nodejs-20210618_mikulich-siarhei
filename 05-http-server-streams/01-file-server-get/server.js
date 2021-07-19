4df3eb7b628697d969ce2b005db9461618d1a030
Siarhei <siarhei.mikulich@Siarheis-MacBook-Pro.local>
2021-07-19 22:11:35 +0300

    [01-file-server-get] using createReadStream in return file

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -12,42 +12,54 @@ const pathHasNesting = (path) => {
   return splitedPath.length >= 2 && !isFile;
 };
 
+const getFile = (req, res, filepath) => {
+  const chunks = [];
+  const stream = fs.createReadStream(filepath);
+
+  stream.on('data', (chunk) => {
+    chunks.push(chunk);
+  });
+
+  stream.on('end', () => {
+    const content = Buffer.concat(chunks).toString('utf-8');
+
+    return res.end(content);
+  });
+
+  stream.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      res.statusCode = 404;
+      return res.end('[404], File not found.');
+    }
+
+    throw new Error();
+  });
+};
+
 server.on('request', (req, res) => {
   try {
     const pathname = url.parse(req.url).pathname.slice(1);
 
     const filepath = path.join(__dirname, 'files', pathname);
 
+    if (pathHasNesting(pathname)) {
+      res.statusCode = 400;
+      return res.end('[400], Nesting is not supported.');
+    }
+
     switch (req.method) {
       case 'GET':
-
-        fs.readFile(filepath, (err, content) => {
-          if (pathHasNesting(pathname)) {
-            res.statusCode = 400;
-            return res.end('[400], File not found.');
-          }
-
-          if (err) {
-            if (err.code === 'ENOENT') {
-              res.statusCode = 404;
-              return res.end('[404], File not found.');
-            }
-
-            res.statusCode = 500;
-            return res.end('[500] Internal Server Error');
-          }
-
-          res.end(content);
-        });
+        getFile(req, res, filepath);
 
         break;
 
       default:
         res.statusCode = 501;
         res.end('Not implemented');
-        server.close();
     }
   } catch (error) {
+    res.statusCode = 500;
+    res.end('[500] Internal Server Error');
     console.error(error);
     server.close();
   }
